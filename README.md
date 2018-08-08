1e404ab5b16700ed251ecffaddb1a9795ba3305a
VuAnhNguyenDuc <51300098@hcmut.edu.vn>
2018-08-09 02:01:26 +0700

    update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # vuanh-portfolio
 This is my personal portfolio
+
+https://gist.github.com/Humayon/eb7eb72c2d4270c605fd76f35696e5db
--- src/main/java/com/vuanhnguyenduc/vuanhportfolio/model/Document.java
+++ src/main/java/com/vuanhnguyenduc/vuanhportfolio/model/Document.java
@@ -1,4 +1,18 @@
 package com.vuanhnguyenduc.vuanhportfolio.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "DOCUMENT")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt",""})
 public class Document {
+
+    //
+
 }
